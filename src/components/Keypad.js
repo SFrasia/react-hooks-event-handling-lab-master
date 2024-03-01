233b52e7b074c9f5b4c4045f9f55c6df0604b694
SFrasia <108404961+SFrasia@users.noreply.github.com>
2024-03-01 16:34:34 +0300

    Changes

---
+++ src/components/Keypad.js
@@ -0,0 +1,13 @@
+// Code Keypad Component Here
+function Keypad() {
+  let change = () => {
+    console.log("Entering password...");
+  };
+  return (
+    <div>
+      <input type="password" onChange={change} />
+    </div>
+  );
+}
+
+export default Keypad;
